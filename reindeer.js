e8633dd9305c0d6b54fe9267dabc26f6b7806057
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-28 10:19:29 -0600

    added nested loop and created unordered list in html to push iterations to

--- reindeer.js
+++ reindeer.js
@@ -3,3 +3,15 @@ var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donne
 
 
 var hohohoElement = document.getElementById("coloredReindeer");
+
+var pairing = function() {
+  var pair = "";
+  for(var i = 0; i < colors.length; i++) {
+    for(var j = 0; j < reindeer.length; j++) {
+      pair += "<li>" + colors[i] + " " + reindeer[j] + " </li>";
+    }
+  }
+  debugger
+  return pair;
+}
+hohohoElement.innerHTML = pairing();
